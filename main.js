fb0a6ee9c385ede0456442af89e591b7e063253c
Mananas <Mananas>
2025-01-13 13:31:52 +0100

    Update js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function getThankYouTemplate(chose = 0) {
     const template = `
    <div class="card">
-        <img src="./layout/illustration-thank-you.svg" alt="imagen de una estrella">
-        <p>Seleccionaste ${chose} de 5</p>
+        <img src="./layout/illustration-thank-you.svg" alt="imagen de una estrella" class="thanks">
+        <p class="selection">Seleccionaste ${chose} de 5</p>
         <h1>!Gracias!</h1>
         <section>
             <p>Agradecemos que te hayas tomado el tiempo de dar una calificación</p>
@@ -11,3 +11,16 @@ function getThankYouTemplate(chose = 0) {
     </div>`;
     return template;
 }
+
+function handlerForm(event) {
+    event.preventDefault();
+    const form = event.target;
+    const option = new FormData(form);
+    const valueOption = option.get("assessment");
+    const insertPoint = document.querySelector("body");
+    const template = getThankYouTemplate(valueOption);
+    insertPoint.innerHTML = template;
+}
+
+const formCapture = document.querySelector("form");
+formCapture.addEventListener("submit", handlerForm);
