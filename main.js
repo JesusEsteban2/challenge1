39a9e5665f280f949885007b43969598060f1d67
jesusesteban2 <jesus.esteban@outlook.com>
2025-01-13 18:42:22 +0100

    Change style for page2

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 function getThankYouTemplate(chose = 0) {
     const template = `
    <div class="card">
-        <img src="./layout/illustration-thank-you.svg" alt="imagen de una estrella" class="thanks">
-        <p class="selection">Seleccionaste ${chose} de 5</p>
-        <h1>!Gracias!</h1>
-        <section>
+        <img src="./layout/illustration-thank-you.svg" alt="imagen de una estrella" class="img-thanks">
+        <p class="selectionChoice">Seleccionaste ${chose} de 5</p>
+        <h2 class="selection">!Gracias!</h2>
+        <section class="selection">
             <p>Agradecemos que te hayas tomado el tiempo de dar una calificación</p>
             <p>Si alguna vez necesitas más ayuda, no dudes en volver a ponerte en contacto con nosotros.</p>
         </section>
@@ -14,6 +14,7 @@ function getThankYouTemplate(chose = 0) {
 
 function handlerForm(event) {
     event.preventDefault();
+
     const form = event.target;
     const option = new FormData(form);
     const valueOption = option.get("assessment");
